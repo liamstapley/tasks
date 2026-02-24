4b3fb45e8a85172907d2516820c15cf51810a601
liamstapley <stapley.liam@gmail.com>
2026-02-24 16:46:09 -0500

    solved task 6: objects

--- src/objects.ts
+++ src/objects.ts
@@ -8,9 +8,18 @@ import { Question, QuestionType } from "./interfaces/question";
 export function makeBlankQuestion(
     id: number,
     name: string,
-    type: QuestionType
+    type: QuestionType,
 ): Question {
-    return {};
+    return {
+        id: id,
+        name: name,
+        body: "",
+        type: type,
+        options: [],
+        expected: "",
+        points: 1,
+        published: false,
+    };
 }
 
 /**
@@ -21,7 +30,9 @@ export function makeBlankQuestion(
  * HINT: Look up the `trim` and `toLowerCase` functions.
  */
 export function isCorrect(question: Question, answer: string): boolean {
-    return false;
+    return (
+        question.expected.trim().toLowerCase() === answer.trim().toLowerCase()
+    );
 }
 
 /**
@@ -31,7 +42,9 @@ export function isCorrect(question: Question, answer: string): boolean {
  * be exactly one of the options.
  */
 export function isValid(question: Question, answer: string): boolean {
-    return false;
+    return question.type === "multiple_choice_question" ?
+            question.options.includes(answer)
+        :   true;
 }
 
 /**
@@ -41,7 +54,7 @@ export function isValid(question: Question, answer: string): boolean {
  * name "My First Question" would become "9: My First Q".
  */
 export function toShortForm(question: Question): string {
-    return "";
+    return question.id.toString() + ": " + question.name.slice(0, 10);
 }
 
 /**
@@ -62,7 +75,11 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-    return "";
+    let markDown: string = "# " + question.name + "\n" + question.body;
+    let answerChoices = "\n- " + question.options.join("\n- ");
+    return question.type === "multiple_choice_question" ?
+            markDown + answerChoices
+        :   markDown;
 }
 
 /**
@@ -70,7 +87,11 @@ export function toMarkdown(question: Question): string {
  * `newName`.
  */
 export function renameQuestion(question: Question, newName: string): Question {
-    return question;
+    return {
+        ...question,
+        name: newName,
+        options: [...question.options],
+    };
 }
 
 /**
@@ -79,7 +100,10 @@ export function renameQuestion(question: Question, newName: string): Question {
  * published; if it was published, now it should be not published.
  */
 export function publishQuestion(question: Question): Question {
-    return question;
+    return {
+        ...question,
+        published: !question.published,
+    };
 }
 
 /**
@@ -89,7 +113,13 @@ export function publishQuestion(question: Question): Question {
  * The `published` field should be reset to false.
  */
 export function duplicateQuestion(id: number, oldQuestion: Question): Question {
-    return oldQuestion;
+    return {
+        ...oldQuestion,
+        id: id,
+        name: "Copy of " + oldQuestion.name,
+        options: [...oldQuestion.options],
+        published: false,
+    };
 }
 
 /**
@@ -100,7 +130,10 @@ export function duplicateQuestion(id: number, oldQuestion: Question): Question {
  * Check out the subsection about "Nested Fields" for more information.
  */
 export function addOption(question: Question, newOption: string): Question {
-    return question;
+    return {
+        ...question,
+        options: [...question.options, newOption],
+    };
 }
 
 /**
@@ -115,7 +148,16 @@ export function mergeQuestion(
     id: number,
     name: string,
     contentQuestion: Question,
-    { points }: { points: number }
+    { points }: { points: number },
 ): Question {
-    return contentQuestion;
+    return {
+        id: id,
+        name: name,
+        body: { ...contentQuestion }.body,
+        type: { ...contentQuestion }.type,
+        options: [...contentQuestion.options],
+        expected: { ...contentQuestion }.expected,
+        points: points,
+        published: false,
+    };
 }
